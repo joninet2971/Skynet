728054e6b67a25bf28fa9e066f887ce55423e2a4
joninet2971 <j.desplats@itecriocuarto.org.ar>
2025-07-24 08:21:17 +0000

    Delete templates airplana

--- skynet_app/airplane/admin.py
+++ skynet_app/airplane/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Airplane, Seat
 
-# Register your models here.
+@admin.register(Airplane)
+class AirplaneAdmin(admin.ModelAdmin):
+    list_display = ('model', 'capacity', 'rows', 'columns')
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('number', 'airplane', 'row', 'column', 'type', 'status')
--- skynet_app/airplane/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Airplane
-
-class AirplaneForm(forms.ModelForm):
-    class Meta:
-        model = Airplane
-        fields = ['model', 'capacity', 'rows', 'columns']
