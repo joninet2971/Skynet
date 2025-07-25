33df4f9a8797a59411786354bdf6bd3957728641
joninet2971 <j.desplats@itecriocuarto.org.ar>
2025-07-25 10:06:48 +0000

    deleted airplane admin

--- skynet_app/airplane/admin.py
+++ skynet_app/airplane/admin.py
@@ -4,7 +4,14 @@ from .models import Airplane, Seat
 @admin.register(Airplane)
 class AirplaneAdmin(admin.ModelAdmin):
     list_display = ('model', 'rows', 'columns')
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 @admin.register(Seat)
 class SeatAdmin(admin.ModelAdmin):
     list_display = ('number', 'airplane', 'row', 'column', 'type', 'status')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
