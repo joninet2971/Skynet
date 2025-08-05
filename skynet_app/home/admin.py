afc134b561802acf74200b15646fd2d6b7616c6a
jonathan <jon.desplats@gmail.com>
2025-08-04 21:11:22 -0300

    create carousel images/media

--- skynet_app/home/admin.py
+++ skynet_app/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from home.models import CarouselImage
 # Register your models here.
+
+
+@admin.register(CarouselImage)
+class CarouselImageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
